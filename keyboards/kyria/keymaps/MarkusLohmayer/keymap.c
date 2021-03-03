f48384545d378630af35a58bdce9f31906790a14
Markus Lohmayer <markus.lohmayer@gmail.com>
2021-03-03 08:44:20 +0100

    move ESC key to thumb

--- keyboards/kyria/keymaps/MarkusLohmayer/keymap.c
+++ keyboards/kyria/keymaps/MarkusLohmayer/keymap.c
@@ -23,13 +23,13 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_B] = LAYOUT(
-  KC_ESC  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                         KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_MINS ,
+  KC_GRV  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                         KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_MINS ,
   _L_TAB  , GUI_A   , ALT_S   , CTL_D   , SFT_F   , _R_G    ,                                         _R_H    , SFT_J   , CTL_K   , ALT_L   , GUI_SCLN, _L_QUOT ,
   KC_NO   , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , OSL(_0) , KC_LEFT , KC_RGHT , KC_NO   , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_BSLS ,
-                                KC_LEAD , TT(_R)  , KC_BSPC , KC_NO   , KC_DOWN , KC_UP   , KC_ENT  , KC_SPC  , TT(_L)  , KC_MUTE
+                                KC_LEAD , TT(_R)  , KC_BSPC , KC_ESC  , KC_DOWN , KC_UP   , KC_ENT  , KC_SPC  , TT(_L)  , KC_MUTE
 ),
 [_L] = LAYOUT(
-  KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                                         KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_EQL  ,
+  _______ , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                                         KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_EQL  ,
   KC_CAPS , _______ , _______ , _______ , _______ , _______ ,                                         _______ , _______ , _______ , _______ , _______ , _______ ,
   _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_LBRC , KC_RBRC , _______ , _______ ,
                                 _______ , _______ , KC_DEL  , _______ , _______ , _______ , _______ , _______ , _______ , _______
