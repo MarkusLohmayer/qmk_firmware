761c25dd28583fc1885acf80d2fab0fbd7225b81
Markus Lohmayer <markus.lohmayer@gmail.com>
2021-05-22 08:49:03 +0200

    use layer switch keys (next to encoders) to switch to layers L / R (and back to B (default))

--- keyboards/kyria/keymaps/MarkusLohmayer/config.h
+++ keyboards/kyria/keymaps/MarkusLohmayer/config.h
@@ -2,6 +2,7 @@
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 
+/* homerow mod */
 #define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
@@ -9,5 +10,9 @@
 
 #define ENCODER_RESOLUTION 2
 
+/* leader key */
 #define LEADER_TIMEOUT 800
 #define LEADER_PER_KEY_TIMING
+
+/* toggle layer via TT and multiple taps */
+#define TAPPING_TOGGLE 2
--- keyboards/kyria/keymaps/MarkusLohmayer/keymap.c
+++ keyboards/kyria/keymaps/MarkusLohmayer/keymap.c
@@ -1,5 +1,14 @@
 #include QMK_KEYBOARD_H
 
+enum layers {
+  _B = 0,
+  _L,
+  _R,
+  _0,
+  _1
+};
+
+/* homerow mod */
 #define _L_TAB   LT(_L, KC_TAB)
 #define GUI_A    LGUI_T(KC_A)
 #define ALT_S    LALT_T(KC_S)
@@ -21,32 +30,24 @@
 #define UM_OE    RALT(KC_P)
 #define EURO     RALT(KC_5)
 
-enum layers {
-  _B = 0,
-  _L,
-  _R,
-  _0,
-  _1
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_B] = LAYOUT(
   KC_GRV  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                                         KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_MINS ,
   _L_TAB  , GUI_A   , ALT_S   , CTL_D   , SFT_F   , _R_G    ,                                         _R_H    , SFT_J   , CTL_K   , ALT_L   , GUI_SCLN, _L_QUOT ,
   KC_LEAD , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , OSL(_0) , KC_LEFT , KC_RGHT , OSL(_1) , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_BSLS ,
-                                KC_NO   , TT(_R)  , KC_BSPC , KC_ESC  , KC_DOWN , KC_UP   , KC_ENT  , KC_SPC  , TT(_L)  , KC_MUTE
+                                KC_NO   , TO(_L)  , KC_BSPC , KC_ESC  , KC_DOWN , KC_UP   , KC_ENT  , KC_SPC  , TO(_R)  , KC_MUTE
 ),
 [_L] = LAYOUT(
   _______ , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                                         KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_EQL  ,
   KC_CAPS , _______ , _______ , _______ , _______ , _______ ,                                         _______ , _______ , _______ , _______ , _______ , _______ ,
   _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_LBRC , KC_RBRC , _______ , _______ ,
-                                _______ , _______ , KC_DEL  , _______ , _______ , _______ , _______ , _______ , _______ , _______
+                                _______ , TO(_B)  , KC_DEL  , _______ , _______ , _______ , _______ , _______ , TO(_B)  , _______
 ),
 [_R] = LAYOUT(
   _______ , _______ , _______ , _______ , _______ , _______ ,                                         _______ , KC_LBRC , KC_RBRC , KC_LPRN , KC_RPRN , _______ ,
   _______ , _______ , _______ , _______ , _______ , _______ ,                                         KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______ , _______ ,
   _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , _______ , _______ ,
-                                _______ , _______ , _______ , _______ , KC_BRID , KC_BRIU , KC_BTN1 , KC_BTN3 , _______ , _______
+                                _______ , TO(_B)  , _______ , _______ , KC_BRID , KC_BRIU , KC_BTN1 , KC_BTN3 , TO(_B)  , _______
 ),
 [_0] = LAYOUT(
   _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                                         KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , _______ ,
